9b83cd619712aa16be9df0f7473d2bf99b8b8552
guicoelhodev <gs.coelho@outlook.com>
2023-08-25 17:30:09 -0300

    feat: create default page route feature

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
   title: "Take a Note",
   srcDir: "./src",
 });
