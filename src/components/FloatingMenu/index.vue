90ceb695d6c5921b082e59787f4403ff5ee45338
guicoelhodev <gs.coelho@outlook.com>
2023-08-26 10:36:46 -0300

    feat: create FloatingMenu component

--- src/components/FloatingMenu/index.vue
+++ src/components/FloatingMenu/index.vue
@@ -2,30 +2,73 @@
   <floating-menu
     :editor="editor"
     :tippy-options="{ duration: 100 }"
+    :shouldShow="
+      ({ state }) => state.selection.$from.nodeBefore?.textContent === '/'
+    "
+    class="bg-zinc-50 p-1 rounded-md shadow-lg flex flex-col items-start"
     v-if="editor"
   >
-    <button
-      @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-      :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+    <ul
+      class="p-0 m-0 flex flex-col gap-[2px] max-h-48 overflow-y-scroll no-scrollbar"
     >
-      H1
-    </button>
-    <button
-      @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
-      :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
-    >
-      H2
-    </button>
-    <button
-      @click="editor.chain().focus().toggleBulletList().run()"
-      :class="{ 'is-active': editor.isActive('bulletList') }"
-    >
-      Bullet List
-    </button>
+      <FloatingMenuButton
+        v-for="(button, index) in buttons"
+        :title="button.title"
+        :subtitle="button.subtitle"
+        :icon="button.icon"
+        @click="button.onClick()"
+      />
+    </ul>
   </floating-menu>
 </template>
 
 <script setup lang="ts">
 import { FloatingMenu } from "@tiptap/vue-3";
 const { editor } = defineProps<{ editor: any }>();
+
+type IButton = {
+  title: string;
+  subtitle: string;
+  icon: string;
+  onClick: () => void;
+};
+
+const buttons: IButton[] = [
+  {
+    title: "Bullets",
+    subtitle: "Create bullet list",
+    icon: "fluent-mdl2:radio-bullet",
+    onClick: () => editor.chain().focus().toggleBulletList().run(),
+  },
+  {
+    title: "Order list",
+    subtitle: "Create an ordered list",
+    icon: "teenyicons:list-ordered-solid",
+    onClick: () => editor.chain().focus().toggleOrderedList().run(),
+  },
+  {
+    title: "Heading 1",
+    subtitle: "Large section title",
+    icon: "lucide:heading-1",
+    onClick: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
+  },
+  {
+    title: "Heading 2",
+    subtitle: "Medium section title",
+    icon: "lucide:heading-2",
+    onClick: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
+  },
+  {
+    title: "Heading 3",
+    subtitle: "Small section title",
+    icon: "lucide:heading-3",
+    onClick: () => editor.chain().focus().toggleHeading({ level: 3 }).run(),
+  },
+  {
+    title: "Code",
+    subtitle: "Create code block",
+    icon: "teenyicons:code-solid",
+    onClick: () => editor.chain().focus().toggleCodeBlock().run(),
+  },
+];
 </script>
