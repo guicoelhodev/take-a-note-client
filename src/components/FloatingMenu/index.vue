cdf8369d198a5bc7dacd5845ab8be66ae9f48cb0
guicoelhodev <gs.coelho@outlook.com>
2023-08-26 08:13:39 -0300

    feat: create base floating menu component

---
+++ src/components/FloatingMenu/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <floating-menu
+    :editor="editor"
+    :tippy-options="{ duration: 100 }"
+    v-if="editor"
+  >
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+    >
+      H1
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
+    >
+      H2
+    </button>
+    <button
+      @click="editor.chain().focus().toggleBulletList().run()"
+      :class="{ 'is-active': editor.isActive('bulletList') }"
+    >
+      Bullet List
+    </button>
+  </floating-menu>
+</template>
+
+<script setup lang="ts">
+import { FloatingMenu } from "@tiptap/vue-3";
+const { editor } = defineProps<{ editor: any }>();
+</script>
