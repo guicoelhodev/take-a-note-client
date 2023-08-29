6f0445b5bc7955a2bca47fd5c4a5860d0fdcf605
guicoelhodev <gs.coelho@outlook.com>
2023-08-28 22:38:30 -0300

    feat: create page to get jwt token

---
+++ src/pages/[login].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Login session</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+onMounted(() => {
+  console.log(route.hash);
+});
+</script>
