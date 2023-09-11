1613e00b70a449df7e577770e19487953f1b8ca9
Guilherme Santos Coelho <gs.coelho@outlook.com>
2023-09-11 15:59:39 -0300

    feat: create TypeWritter component

---
+++ src/components/TypeWritter/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <article>
+  </article>
+</template>
+
+<script setup lang="ts">
+const { text } = defineProps({
+  text: String,
+  class: String,
+})
+</script>
+
+<style scoped>
+  .typingEffect {
+    width: 0;
+    overflow: hidden; 
+    border-right: 2px solid white;
+    font-size: 16px;
+    white-space: nowrap;
+    animation: typing 20s steps(30) forwards, blink 1s infinite;
+  }
+
+@keyframes typing {
+  from { width: 0 }
+  to { width: 100% }
+}
+
+@keyframes blink {
+  0%, 45% {
+    border-color: transparent;
+  }
+  50%, 100% {
+    border-color: white;
+  }
+}
+</style>
