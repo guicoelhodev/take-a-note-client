cb70735fd4d915506b253ac827555f4a2a0c1a48
Guilherme Santos Coelho <gs.coelho@outlook.com>
2023-09-11 16:01:00 -0300

    feat: add default page to simulate user signed

---
+++ src/pages/app.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// const query = gql`
+//   query users {
+//     createdAt
+//     name
+//   }
+// `;
+const query = gql`
+  query Query {
+    users {
+      name
+    }
+  }
+`;
+
+const { data } = await useAsyncQuery<{ users: { name: string }[] }>(query);
+
+onMounted(() => {
+  console.log("oi", data);
+});
+</script>
+<template>
+  <ul>
+    <li v-for="user in data.users" :key="user.name">{{ user.name }}</li>
+  </ul>
+  <Editor />
+</template>
