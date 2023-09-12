e7602a939275f6bcbc44e4adbd6e562ac9e9d2fc
guicoelhodev <gs.coelho@outlook.com>
2023-09-11 22:05:55 -0300

    feat: add validation to verify if user has data into pinea state managment

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex justify-between gap-4">
-    <nav class="p-4 border border-red-300 max-w-[140px] w-full">
+  <div class="flex justify-between gap-4 overflow-hidden">
+    <nav class="p-4 border border-red-300 max-w-[200px] w-full">
       <h3>Navigation</h3>
 
       <ul>
@@ -14,9 +14,7 @@
       <slot />
     </main>
 
-    <aside class="p-4 border border-red-300 max-w-[280px] w-full">
-      {{ userStore.user ?? "Sem dados" }}
-    </aside>
+    <UserMenu :user="userStore.user" />
   </div>
 </template>
 
@@ -24,7 +22,36 @@
 import { useUserStore } from "~/store/UserStore";
 
 const userStore = useUserStore();
-// const user = computed(() => userStore.user);
+const { mutate: fetchUserLogin, error } = useMutation(
+  gql`
+    mutation LogIn($jwtToken: String!) {
+      logIn(jwtToken: $jwtToken) {
+        avatar_url
+        email
+        full_name
+        user_name
+        id
+      }
+    }
+  `
+);
 
-onMounted(() => {});
+const getUserInfoBasedOnSavedToken = async () => {
+  if (!localStorage.getItem("@take_a_note:token")) return navigateTo("/");
+
+  if (userStore.user) return;
+
+  const userData = await fetchUserLogin({
+    jwtToken: localStorage.getItem("@take_a_note:token"),
+  }).catch((error) => {
+    alert(error);
+    navigateTo("/");
+    return;
+  });
+
+  userStore.handleUserInfo(userData?.data.logIn);
+};
+onMounted(async () => {
+  await getUserInfoBasedOnSavedToken();
+});
 </script>
