b77698d776f99a65eb1137d0407da2da7186b806
guicoelhodev <gs.coelho@outlook.com>
2023-10-28 23:26:42 -0300

    feat: create production page to show demo for users

---
+++ src/pages/production.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="max-w-[1366px] w-full mx-auto flex flex-col p-4 gap-4">
+    <aside class="self-end" @click="navigateTo('/')">
+      <button class="bg-zinc-800 text-white py-2 px-4 rounded-md">
+        Go to home page
+      </button>
+    </aside>
+
+    <Editor />
+  </section>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+});
+</script>
